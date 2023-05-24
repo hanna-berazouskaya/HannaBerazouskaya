50bca851a91055033f26c7753678ebf125007a13
Hanna Berazouskaya <bereza1698@gmail.com>
2023-05-24 22:55:06 +0300

    Added a solution to the problem

---
+++ codewars/Credit_Card_Mask.js
@@ -0,0 +1,26 @@
+// return masked string
+function maskify(cc) {
+    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+    }
+
+// return masked string
+function maskify(cc) {
+    if (cc.length > 4) {
+        let result = '';
+        return cc.split('').reduce((acc, cur, i) => {
+          if (cc.length - i > 4) {
+            result += '#';
+          } else {
+            result += cur;
+          }
+          return result;
+        }, '');
+      } else {
+        return cc;
+      }
+    }
+
+// return masked string
+function maskify(cc) {
+    return cc.replace(/.(?=.{4})/g, "#");
+    }
