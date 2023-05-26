ad2d2910c4e1e2ef35bca9d8dcd5bc89c32141c6
Hanna Berazouskaya <bereza1698@gmail.com>
2023-05-26 23:16:48 +0300

    I have made additions

---
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
@@ -0,0 +1,58 @@
+// 1. Comment Your JavaScript Code
+/* Comment Your JavaScript Code 
+Comment Your JavaScript Code 
+Comment Your JavaScript Code 
+Comment Your JavaScript Code 
+*/
+
+// 2. Declare JavaScript Variables
+
+var myName;
+
+// 3. Storing Values with the Assignment Operator
+
+var a;
+a = 5;
+
+// 4. Assigning the Value of One Variable to Another
+
+// Setup
+var a;
+a = 7;
+var b;
+b=a;
+// Only change code below this line
+
+// 5. Initializing Variables with the Assignment Operator
+
+var a = 9;
+
+// 6. Declare String Variables
+
+var myFirstName = "Hanna";
+var myLastName = "Berazouskaya";
+
+// 7. Understanding Uninitialized Variables
+
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// 8. Understanding Case Sensitivity in Variables
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
