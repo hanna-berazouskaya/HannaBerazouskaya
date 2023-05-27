ef0d0886effad391222f8348d82882107b98f750
Hanna Berazouskaya <bereza1698@gmail.com>
2023-05-27 17:13:16 +0300

    revise

---
+++ codewars/7_kyu/all-star-code-challenge16.js
@@ -0,0 +1,12 @@
+/* DESCRIPTION:
+This Kata is intended as a small challenge for my students
+All Star Code Challenge #16
+Create a function called noRepeat() that takes a string argument and returns a single letter string of the first not repeated character in the entire string. */
+
+// Option 1
+
+function noRepeat(str) {  
+    for (var i of str )
+    if (str.split(i).length ==2 )
+    return i
+  }
