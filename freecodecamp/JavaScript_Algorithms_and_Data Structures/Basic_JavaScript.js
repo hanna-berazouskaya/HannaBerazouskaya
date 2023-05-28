82807fddabbf51fe2a416b9cdb5d3e9193fac76a
Hanna Berazouskaya <bereza1698@gmail.com>
2023-05-28 17:41:50 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
@@ -179,3 +179,56 @@ const remainder = 11%3;
 
 // 21. Compound Assignment With Augmented Addition
 
+/* Convert the assignments for a, b, and c to use the += operator. */
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// 22. Compound Assignment With Augmented Subtraction
+
+/* Convert the assignments for a, b, and c to use the -= operator. */
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 23. Compound Assignment With Augmented Multiplication
+
+/* Convert the assignments for a, b, and c to use the *= operator. */
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+// 24. Compound Assignment With Augmented Division
+
+/* Convert the assignments for a, b, and c to use the /= operator. */
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 24. Escaping Literal Quotes in Strings
+
+/* */
