fbfffc70787825ea8975b99c5944b3ab00f2f58a
Hanna Berazouskaya <bereza1698@gmail.com>
2023-05-28 20:02:00 +0300

    add

---
+++ codewars/7_kyu/all-star-code-challenge22.js
@@ -0,0 +1,31 @@
+/* All Star Code Challenge #22
+
+Create a function that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
+
+Any remaining seconds left over are ignored.
+
+Note:
+The string output needs to be in the specific form - "X hour(s) and X minute(s)"
+
+For example:
+
+3600 --> "1 hour(s) and 0 minute(s)"
+3601 --> "1 hour(s) and 0 minute(s)"
+3500 --> "0 hour(s) and 58 minute(s)"
+323500 --> "89 hour(s) and 51 minute(s)" */
+
+// Option 1
+
+function toTime(time) {
+    var h = Math.floor(time / 3600);
+    var m = Math.floor(time % 3600 / 60);
+    return h + " hour(s) and " + m + " minute(s)";
+  }
+
+// Option 2
+
+function toTime(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor(seconds % 3600 / 60);
+    return `${hours} hour(s) and ${minutes} minute(s)`
+  }
