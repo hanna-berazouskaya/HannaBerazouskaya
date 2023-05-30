239d7d4905d81f5f8e881ef2959f6ccab6bf9675
Hanna Berazouskaya <bereza1698@gmail.com>
2023-05-30 21:51:56 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
@@ -231,4 +231,16 @@ c /= 11;
 
 // 24. Escaping Literal Quotes in Strings
 
-/* */
+/* Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+
+I am a "double quoted" string inside "double quotes". */
+
+//const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+// 25. Quoting Strings with Single Quotes
+
+/* Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+Right now, the <a> tag in the string uses double quotes everywhere. You will need to change the outer quotes to single quotes so you can remove the escape characters. */
+
+const myStr = '<a href="http://www.example.com" target="_blank"> Link </a>';
