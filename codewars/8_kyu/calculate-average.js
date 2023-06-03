bfdc256552f89017e140dfefb8c32359e6240c8c
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-04 00:13:35 +0300

    add

---
+++ codewars/8_kyu/calculate-average.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION:
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0. */
+
+//Option 1
+var find_average = (array) => {  
+  return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+}
