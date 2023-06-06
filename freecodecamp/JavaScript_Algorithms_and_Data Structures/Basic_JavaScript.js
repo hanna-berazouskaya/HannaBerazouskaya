a2333e756d642d9b6abb43e818f0929b5e36744f
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-06 23:03:44 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
@@ -999,3 +999,16 @@ Make an object that represents a dog called myDog which contains the properties 
 
 You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array. */
 
+const myDog = {
+  // Only change code below this line
+  name: "Whiskers",
+  legs: 4,
+  tails: 1,
+  friends: ["Water", "Dogs"]
+
+  // Only change code above this line
+};
+
+/* 84. Accessing Object Properties with Dot Notation
+Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt. */
+
