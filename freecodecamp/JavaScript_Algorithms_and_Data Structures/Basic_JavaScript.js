8d87826b139553a6a62b6341246c2758faad6d3e
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-08 23:03:30 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
@@ -1095,5 +1095,23 @@ delete myDog.tails;*/
 
 Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
 
-*/
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+result = lookup [val];
+  // Only change code above this line
+return result;
+}
+
+phoneticLookup("charlie"); */
 
