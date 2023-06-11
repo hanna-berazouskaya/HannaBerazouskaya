3959dc2cac68e98cc8558e8aa43c7efb69cf0867
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-11 21:17:51 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/Basic_JavaScript.js
@@ -1289,3 +1289,48 @@ for(let i=9; i > 0; i-=2){
   myArray.push(i);
 } */
 
+/* 99. Iterate Through an Array with a For Loop
+Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total. 
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+let total=0;
+for (let i = 0; i < myArr.length; i++) {
+   total += myArr[i];
+}
+*/
+
+/* 100. Nesting For Loops
+Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+function multiplyAll(arr) {
+  let product = 1;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+  }
+}
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+*/
+
+/* 101. Iterate with JavaScript Do...While Loops
+
+Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+
+// Setup
+const myArray = [];
+let i = 10;
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
+
+*/
+
+/* 102. Replace Loops using Recursion
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+ */
