4590adf763cbdaabeb112e5c480d7b9408d56b33
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-19 21:47:20 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
@@ -4,14 +4,54 @@ Fix the code so that i declared in the if statement is a separate variable than 
 This exercise is designed to illustrate the difference between how var and let keywords assign scope to the declared variable. When programming a function similar to the one used in this exercise, it is often better to use different variable names to avoid confusion.
 
 function checkScope() {
-  let i = 'function scope';
-  if (true) {
+let i = 'function scope';
+if (true) {
     let i = 'block scope';
     console.log('Block scope i is: ', i);
-  }
-  console.log('Function scope i is: ', i);
-  return i;
+}
+console.log('Function scope i is: ', i);
+return i;
 } */
 
 /* 2. Mutate an Array Declared with const 
+An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
+const s = [5, 7, 2];
+function editInPlace() {
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+return s;
+}
+editInPlace();
 */
+
+/* 3. Prevent Object Mutation
+In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
+function freezeObj() {
+const MATH_CONSTANTS = {
+    PI: 3.14
+};
+Object.freeze(MATH_CONSTANTS);
+try {
+    MATH_CONSTANTS.PI = 99;
+} catch(ex) {
+    console.log(ex);
+}
+return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+*/
+
+/* 4. Use Arrow Functions to Write Concise Anonymous Functions
+Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var.
+
+const magic = () => new Date();
+*/
+
+/* 5. Write Arrow Functions with Parameters
+Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5])); */
+
+/* 6. */
