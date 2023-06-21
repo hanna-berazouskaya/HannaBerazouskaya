cb386b155c9391bc5751977d077dd45a2d518721
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-21 23:33:52 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
@@ -54,4 +54,13 @@ const myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 console.log(myConcat([1, 2], [3, 4, 5])); */
 
-/* 6. */
+/* 6. Use the Rest Parameter with Function Parameters
+Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+const sum = (...args) => {
+let total = 0;
+for (let i = 0; i < args.length; i++) {
+    total += args[i];
+}
+return total;
+}
+ */
