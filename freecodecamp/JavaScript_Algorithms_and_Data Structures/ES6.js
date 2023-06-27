455acefeedb835830ba6a72f3e7b96ce6104f694
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-27 21:37:25 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
@@ -103,5 +103,7 @@ tomorrow: { low: 68, high: 80 }
 const {today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
 
 11. Use Destructuring Assignment to Assign Variables from Arrays
-
+Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+let a = 8, b = 6;
+[a, b] = [b, a];
 */
