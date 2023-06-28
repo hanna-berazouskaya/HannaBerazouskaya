d74f43882f06c7ee69b05a76c2718cb99ad2f162
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-28 23:42:20 +0300

    add

---
+++ codewars/8_kyu/drink-about.js
@@ -0,0 +1,19 @@
+/* DESCRIPTION:
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples: (Input --> Output)*/
+
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
