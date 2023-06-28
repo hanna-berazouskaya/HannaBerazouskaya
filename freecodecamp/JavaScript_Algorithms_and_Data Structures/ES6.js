d390f3ed774bc5c2c80ad3bf589d9ced327d5b19
Hanna Berazouskaya <bereza1698@gmail.com>
2023-06-28 23:36:03 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
@@ -106,4 +106,18 @@ const {today: { low: lowToday, high: highToday }} = LOCAL_FORECAST;
 Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
 let a = 8, b = 6;
 [a, b] = [b, a];
-*/
+
+
+12. Destructuring via rest elements
+
+Use a destructuring assignment with the rest syntax to emulate the behavior of Array.prototype.slice(). removeFirstTwo() should return a sub-array of the original array list with the first two elements omitted.
+
+function removeFirstTwo(list) {
+const [a, b, ...shorterList] = list;
+return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+13. */
