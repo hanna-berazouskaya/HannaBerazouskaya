f5574842e8c5db0beef7a3352878c23a8f881236
Hanna Berazouskaya <bereza1698@gmail.com>
2023-07-05 22:46:36 +0300

    add

--- freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
+++ freecodecamp/JavaScript_Algorithms_and_Data Structures/ES6.js
@@ -165,4 +165,16 @@ function makeList(arr) {
   return failureItems;
 }
 
-const failuresList = makeList(result.failure);*/
+const failuresList = makeList(result.failure);
+
+15. Write Concise Object Literal Declarations Using Object Property Shorthand
+
+Use object property shorthand with object literals to create and return an object with name, age and gender properties.
+
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return ({name,age,gender});
+  // Only change code above this line
+};
+
+*/
